19723be871997077cc2d685b007033fc038822d2
Elexior <elexior@gmail.com>
2025-09-12 02:40:56 +0200

    Update TheGreatIceflowRelay.lua

--- TheGreatIceflowRelay.lua
+++ TheGreatIceflowRelay.lua
@@ -132,47 +132,24 @@ local function CheckCheckpoint()
     end
 end
 
--- Ball check
-local function CheckBallInInventory(autoMode)
+-- Ball check code
+local function CheckBallInInventory()
     local foundBall = false
     for b = 0, NUM_BAG_SLOTS do
         local slots = GetContainerNumSlots(b)
-        if slots and slots > 0 then
-            for s = 1, slots do
-                local link = GetContainerItemLink(b, s)
-                if link and string.find(link, BALL_NAME) then
-                    foundBall = true
-                    break
-                end
+        for s = 1, slots do
+            local link = GetContainerItemLink(b, s)
+            if link and string.find(link, "Heavy Leather Ball") then
+                foundBall = true
+                ballTimer = ballTimer + 1
+                DEFAULT_CHAT_FRAME:AddMessage("Pass it quick! [" .. ballTimer .. "]")
+                break
             end
         end
         if foundBall then break end
     end
-
-    if autoMode then
-        if not runActive then return end
-        if foundBall then
-            totalBallTime = totalBallTime + 1
-            if not hasBall then
-                hasBall = true
-                RelayLocalMessage("You received a Heavy Leather Ball! Timer started.")
-            else
-                RelayLocalMessage("You currently have the Heavy Leather Ball! Time = " .. totalBallTime .. " sec")
-            end
-        else
-            if hasBall then
-                hasBall = false
-                RelayLocalMessage("You no longer have the Heavy Leather Ball.")
-            end
-        end
-    else
-        if foundBall then
-            RelayLocalMessage("You currently have a Heavy Leather Ball. Total tracked time: " .. totalBallTime .. " sec")
-        else
-            RelayLocalMessage("You do NOT have a Heavy Leather Ball in your inventory.")
-        end
-    end
 end
+
 
 ------------------------------------------------------------
 -- FRAME SCRIPT
